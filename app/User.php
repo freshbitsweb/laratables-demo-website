9670ea80806d82dbc32eee034a75cf93782712be
Rajan <rajan@freshbits.in>
2019-12-02 10:25:28 +0530

    users table migrate
    Signed-off-by: Rajan <rajan@freshbits.in>

--- app/User.php
+++ app/User.php
@@ -11,29 +11,9 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Indicates if the model should be timestamped.
      *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+     * @var bool
+    */
+    public $timestamps = false;
 }
