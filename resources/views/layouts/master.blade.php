62683ccd9fc6ee96d18a828814512ef1ffe1a490
Rajan <rajan@freshbits.in>
2019-12-05 18:12:32 +0530

    add font awesome library.
    Signed-off-by: Rajan <rajan@freshbits.in>

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,10 @@
 
     <link rel="stylesheet" href="css/prism_php.css">
 
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css"
+    >
+
     <style>
         #example-name-display {
             color: orangered;
